64a1d3205741b4d6a3bc27a2d57d13b697db5495
RIKO-99 <princericko6@gmal.com>
2023-03-02 13:14:47 -0800

    perfect tree

--- binary_trees.h
+++ binary_trees.h
@@ -24,7 +24,7 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-/*prototype*/
+            /*prototypes*/
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *);
@@ -45,5 +45,6 @@ int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
-binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)i;
+
 #endif
